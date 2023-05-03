66399acbd526eeb359d36a715ac1199c647c59c4
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-05-03 03:18:22 +0200

    test-func

--- src/index.js
+++ src/index.js
@@ -15,27 +15,65 @@ const listEl = document.querySelector('.country-list')
 const divEl = document.querySelector('.country-info')
 
 
-const countriesSearch = () => {
-    fetchCountries.map(value) 
+
+
+
+// const markupInfo
+
+
+const addElList = e => {
+    const countriesSearch = e.target.value.trim();
+
+    const markupList = `<li>${inputEl.value}</li>`
+    listEl.innerHTML = 'lll';
+
+    if (countriesSearch !== '') {
+        // fetchCountries.map 
+
+
+    }
 }
 
- function testMetod () {
-    setTimeout(() => {
-        const listElBase = document.createElement('li')
-            listElBase.innerHTML = inputEl.value
-            listEl.appendChild(listElBase) 
 
-    }, DEBOUNCE_DELAY)
- }
 
-inputEl.addEventListener('input', testMetod)
+inputEl.addEventListener('input', addElList)
+
+
+// const listInfo
+
+///// test methods //////////////
 
 
 
+        // console.log(markupList)
+        // listEl.innerHTML = markupList
+        // // listEl.innerHTML = ` <li>${inputEl.value}`
+        // // console.log(fetchCountries())
 
 
 
 
+// const countriesSearch = () => {
+//     fetchCountries.map(value) 
+// }
+
+//  function testMetod () {
+//     setTimeout(() => {
+//         const listElBase = document.createElement('li')
+//             listElBase.innerHTML = inputEl.value
+//             listEl.appendChild(listElBase) 
+
+//     }, DEBOUNCE_DELAY)
+//  }
+
+// inputEl.addEventListener('input', testMetod)
+
+
+// console.log(fetchCountries)
+
+
+
+///////////////////////
 
 
 
