37d08770bce79a84f5b9d9cb21ddf69145506028
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-05-08 03:15:37 +0200

    err-test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 import './css/styles.css';
 
 //biblioteki// notiflix // lodash //
@@ -25,7 +27,9 @@ const searchCountryFunction = e => {
 
     if (searchCountries !== '') {
       fetchCountries(searchCountries)
+
         .then(data => {
+          console.log(data)
             if (2 >= data.length && data.length <= 10) {
               const listCountry = data
                 .map(({name, flag}) => 
@@ -59,113 +63,113 @@ inputEl.addEventListener('input', debounce(searchCountryFunction, DEBOUNCE_DELAY
 
 
 
-/////////////////...................................//////////////////////////////
+// /////////////////...................................//////////////////////////////
 
 
-// // inputEl.addEventListener('input', e => {
-// function searchingCountryInfo () { 
-//     const searchCountries = e.currentTarget.value.trim(); //target//
-//     listEl.innerHTML ='';
+// // // inputEl.addEventListener('input', e => {
+// // function searchingCountryInfo () { 
+// //     const searchCountries = e.currentTarget.value.trim(); //target//
+// //     listEl.innerHTML ='';
 
-//     // if (searchCountries !== '') {
-//         fetchCountries(searchCountries)
-//         .then(data => {
-//             if (data.length >= 20) { 
-//             const markup = elements
-//             .map((element) => {
-//                 return 
-//                 `<li>${element.name.official}
-//                 ${element.flags.svg}
-//                 </li>`
-//             })
-//             .join("")
-//             listEl.innerHTML = markup;
+// //     // if (searchCountries !== '') {
+// //         fetchCountries(searchCountries)
+// //         .then(data => {
+// //             if (data.length >= 20) { 
+// //             const markup = elements
+// //             .map((element) => {
+// //                 return 
+// //                 `<li>${element.name.official}
+// //                 ${element.flags.svg}
+// //                 </li>`
+// //             })
+// //             .join("")
+// //             listEl.innerHTML = markup;
         
-//             }
-//             else if (searchCountries >=2 && searchCountries <=10) {
-//                 console.log('ghghghgg')
-//             }
+// //             }
+// //             else if (searchCountries >=2 && searchCountries <=10) {
+// //                 console.log('ghghghgg')
+// //             }
 
-//         })
-//         .catch(error => {
-//             alert('kkjljll')
-//         })
-//     }
+// //         })
+// //         .catch(error => {
+// //             alert('kkjljll')
+// //         })
+// //     }
+// // // }
+
+// // inputEl.addEventListener('input', searchingCountryInfo)
+
+
+
+
+
+
+// // const markupInfo
+// // {name.official, capital, population, flags, languages}
+
+// // const addElList = e => {
+// //     const countriesSearch = e.target.value.trim();
+
+// //     const markupList = `<li>${inputEl.value}</li>`
+// //     listEl.innerHTML = 'lll';
+
+// //     if (countriesSearch !== '') {
+// //         // fetchCountries.map 
+
+
+// //     }
 // // }
 
-// inputEl.addEventListener('input', searchingCountryInfo)
+
+
+// // inputEl.addEventListener('input', addElList)
+
+
+// // const listInfo
+
+// ///// test methods //////////////
+
+
+
+//         // console.log(markupList)
+//         // listEl.innerHTML = markupList
+//         // // listEl.innerHTML = ` <li>${inputEl.value}`
+//         // // console.log(fetchCountries())
 
 
 
 
+// // const countriesSearch = () => {
+// //     fetchCountries.map(value) 
+// // }
+
+// //  function testMetod () {
+// //     setTimeout(() => {
+// //         const listElBase = document.createElement('li')
+// //             listElBase.innerHTML = inputEl.value
+// //             listEl.appendChild(listElBase) 
+
+// //     }, DEBOUNCE_DELAY)
+// //  }
+
+// // inputEl.addEventListener('input', testMetod)
 
 
-// const markupInfo
-// {name.official, capital, population, flags, languages}
-
-// const addElList = e => {
-//     const countriesSearch = e.target.value.trim();
-
-//     const markupList = `<li>${inputEl.value}</li>`
-//     listEl.innerHTML = 'lll';
-
-//     if (countriesSearch !== '') {
-//         // fetchCountries.map 
-
-
-//     }
-// }
-
-
-
-// inputEl.addEventListener('input', addElList)
-
-
-// const listInfo
-
-///// test methods //////////////
+// // console.log(fetchCountries)
 
 
 
-        // console.log(markupList)
-        // listEl.innerHTML = markupList
-        // // listEl.innerHTML = ` <li>${inputEl.value}`
-        // // console.log(fetchCountries())
+// ///////////////////////
 
 
 
 
-// const countriesSearch = () => {
-//     fetchCountries.map(value) 
-// }
-
-//  function testMetod () {
-//     setTimeout(() => {
-//         const listElBase = document.createElement('li')
-//             listElBase.innerHTML = inputEl.value
-//             listEl.appendChild(listElBase) 
-
-//     }, DEBOUNCE_DELAY)
-//  }
-
-// inputEl.addEventListener('input', testMetod)
-
-
-// console.log(fetchCountries)
-
-
-
-///////////////////////
-
-
-
-
-// inputEl.addEventListener('input', (e) => {
+// // inputEl.addEventListener('input', (e) => {
     
-// })
+// // })
 
 
-////........................................///////////////////////...........///////////////////
+// ////........................................///////////////////////...........///////////////////
 
 
 
